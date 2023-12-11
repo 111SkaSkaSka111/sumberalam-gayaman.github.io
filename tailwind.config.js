3e324bf36219ad974aab98262f466fcef5356c3e
sumberalam <skareggae1998@gmail.com>
2023-12-11 08:50:47 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["index.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                merriweather: "Merriweather",
+            },
+
+            backgroundImage: {
+                heroimage: "url('/dist/img/bg.jpg')",
+            },
+
+            colors: {
+                primary: "#84cc16",
+                dark: "#0f172a",
+                gold: "#eab308",
+                secondary: "#14b8a6",
+            },
+
+            boxShadow: {
+                full: "0px 0px 80px -10px #eab308",
+            },
+        },
+    },
+    plugins: [],
+};
